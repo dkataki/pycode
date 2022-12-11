e54286264389839500a014a5e64f7b126dbc1c22
dkataki <debajitkataki@gmail.com>
2022-12-11 14:06:10 +0530

    bmi code checked in

---
+++ BMI_calculator.py
@@ -0,0 +1,7 @@
+#This is a body mass calculator 
+print("Please enteryour hight in KG: ")
+weight=int(input())
+print("Your height pls : (in meters )")
+height=float(input())
+BMI=weight/height ** 2
+print("YOU BMI is:",BMI)
