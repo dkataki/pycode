dc90a7fa8b9f0563b470ce4e3997602c6bac9722
dkataki <debajitkataki@gmail.com>
2022-12-11 12:15:25 +0530

    final band name code

---
+++ band_name_generator.py
@@ -0,0 +1,7 @@
+#This is a band name generatot code
+print("WELCOME TO THE BAND NAME GENERATOR!!")
+city = input("Which city did you born: \n")
+pet = input("whats the name of your pet: \n")
+print("You probable band name could be :" +city+ "" +pet)
+print("ENJOY!")
+
