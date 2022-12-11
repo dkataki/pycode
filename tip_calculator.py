358ab90b84ca1f1c6e243193c1627850b26190da
dkataki <debajitkataki@gmail.com>
2022-12-11 13:25:54 +0530

    new codes

---
+++ tip_calculator.py
@@ -0,0 +1,14 @@
+#This is a tip calculator
+print("ENter a two digit no:\n")
+a=input()
+
+first=int(int(a)/10)
+
+print("First number is : " ,first)
+
+# second = 2
+
+second = int(a[1])
+print("second number is : ",second)
+print(first + second)
+
